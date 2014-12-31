0bf5d972746207158147da9f078fd773dabe06b9
Frank Oh <kenshin579@hotmail.com>
2014-12-31 14:24:22 +0900

    소스 업데이트

--- powermock/src/test/java/packt/ch1/EmployeeControllerTest.java
+++ powermock/src/test/java/packt/ch1/EmployeeControllerTest.java
@@ -14,6 +14,58 @@ import org.powermock.api.mockito.PowerMockito;
  * @author Deep Shah
  */
 public class EmployeeControllerTest {
+    //Simple Example
+    @Test
+    public void shouldGetCountOfEmployees() {
+        EmployeeController employeeController = new EmployeeController(new EmployeeService());
+        Assert.assertEquals(10, employeeController.getProjectedEmployeeCount());
+    }
+
+    /**
+     * MockSettings은 거의 사용하지 않음
+     * - 하지만, debugging할때 logging(debug)로 더 자세히 프린트하도록 할 수 있음
+     */
+    @Test
+    public void shouldInvokeSaveEmployeeOnTheServiceWhileSavingTheEmployeeWithMockSettings() {
+        EmployeeService mock = PowerMockito.mock(EmployeeService.class, Mockito
+                .withSettings()
+                .name("EmployeeServiceMock")
+                .verboseLogging());
+
+        EmployeeController employeeController = new EmployeeController(mock);
+
+        Employee employee = new Employee();
+        employeeController.saveEmployee(employee);
+
+        //Verifying that controller did call the
+        //saveEmployee method on the mocked service instance.
+        Mockito.verify(mock).saveEmployee(employee);
+    }
+
+    @Test
+    public void shouldInvokeSaveEmployeeOnTheServiceWhileSavingTheEmployee() {
+        EmployeeService mock = PowerMockito.mock(EmployeeService.class);
+
+        EmployeeController employeeController = new EmployeeController(mock);
+
+        Employee employee = new Employee();
+        employeeController.saveEmployee(employee);
+
+        //Verifying that controller did call the
+        //saveEmployee method on the mocked service instance.
+        Mockito.verify(mock).saveEmployee(employee);
+    }
+
+    @Test
+    public void shouldReturnProjectedCountOfEmployeesFromTheServiceRoundedToTheCeiling() {
+        //Creating a mock using the PowerMockito.mock method for the EmployeeService class.
+        EmployeeService mock = PowerMockito.mock(EmployeeService.class);
+
+        PowerMockito.when(mock.getEmployeeCount()).thenReturn(6);
+
+        EmployeeController employeeController = new EmployeeController(mock);
+        Assert.assertEquals(8, employeeController.getProjectedEmployeeCount());
+    }
 
     @Test
     public void shouldReturnProjectedCountOfEmployeesFromTheService() {
@@ -28,17 +80,9 @@ public class EmployeeControllerTest {
         Assert.assertEquals(10, employeeController.getProjectedEmployeeCount());
     }
 
-    @Test
-    public void shouldReturnProjectedCountOfEmployeesFromTheServiceRoundedToTheCeiling() {
-        //Creating a mock using the PowerMockito.mock method for the EmployeeService class.
-        EmployeeService mock = PowerMockito.mock(EmployeeService.class);
-
-        PowerMockito.when(mock.getEmployeeCount()).thenReturn(6);
-
-        EmployeeController employeeController = new EmployeeController(mock);
-        Assert.assertEquals(8, employeeController.getProjectedEmployeeCount());
-    }
-
+    /**
+     * 이건 어떻게 동작하는지 이해가 잘 안됨
+     */
     @Test
     public void shouldReturnCountOfEmployeesFromTheServiceWithDefaultAnswer() {
         //Creating a mock using the PowerMockito.mock method for the EmployeeService class.
@@ -65,34 +109,5 @@ public class EmployeeControllerTest {
         Assert.assertEquals(12, employeeController.getProjectedEmployeeCount());
     }
 
-    @Test
-    public void shouldInvokeSaveEmployeeOnTheServiceWhileSavingTheEmployee() {
-        EmployeeService mock = PowerMockito.mock(EmployeeService.class);
 
-        EmployeeController employeeController = new EmployeeController(mock);
-
-        Employee employee = new Employee();
-        employeeController.saveEmployee(employee);
-
-        //Verifying that controller did call the
-        //saveEmployee method on the mocked service instance.
-        Mockito.verify(mock).saveEmployee(employee);
-    }
-
-    @Test
-    public void shouldInvokeSaveEmployeeOnTheServiceWhileSavingTheEmployeeWithMockSettings() {
-        EmployeeService mock = PowerMockito.mock(EmployeeService.class, Mockito
-                .withSettings()
-                .name("EmployeeServiceMock")
-                .verboseLogging());
-
-        EmployeeController employeeController = new EmployeeController(mock);
-
-        Employee employee = new Employee();
-        employeeController.saveEmployee(employee);
-
-        //Verifying that controller did call the
-        //saveEmployee method on the mocked service instance.
-        Mockito.verify(mock).saveEmployee(employee);
-    }
 }
