a4b376c8dd14534deccdbdd75b52dc0412206c31
kenshin579 <kenshin579@hotmail.com>
2015-01-02 22:37:01 +0900

    ch12: suppressing unwanted behavior

---
+++ powermock/src/main/java/packt/ch12/BaseEntity.java
@@ -0,0 +1,38 @@
+package packt.ch12;
+
+/**
+ * The base class which
+ * is going to do things
+ * that would make its derived classes difficult to test.
+ *
+ * @author Deep Shah
+ */
+public class BaseEntity {
+
+    private final long identifier = 1 / 0;
+
+    /**
+     * Static initializer that will throw a NullPointerException.
+     */
+    static {
+        String x = null;
+        x.toString();
+    }
+
+    /**
+     * The default constructor throws UnsupportedOperationException.
+     */
+    public BaseEntity() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * This method is responsible to do write audit trails.
+     * Currently this method throws an UnsupportedOperationException.
+     *
+     * @param auditInformation the audit information to log.
+     */
+    protected void performAudit(String auditInformation) {
+        throw new UnsupportedOperationException();
+    }
+}
